0174393ec96c9a9c729109468772b61c47ab6c6b
Tony Robalik <tony@gradle.com>
2018-11-27 14:36:40 -0800

    First commit

---
+++ app/src/main/java/com/example/tony/cachingandroidprojects/AppClassA.java
@@ -0,0 +1,10 @@
+package com.example.tony.cachingandroidprojects;
+
+import com.example.library1.LibraryClassA;
+
+public class AppClassA {
+
+    public void hello() {
+        new LibraryClassA().doAThing();
+    }
+}
---
+++ library1/src/main/java/com/example/library1/LibraryClassA.java
@@ -0,0 +1,8 @@
+package com.example.library1;
+
+public class LibraryClassA {
+
+    public void doAThing() {
+        System.out.println("hello");
+    }
+}
